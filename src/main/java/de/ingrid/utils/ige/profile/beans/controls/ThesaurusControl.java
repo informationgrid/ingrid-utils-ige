9419fc578903c0f9766e2588a071dd6e4eaa336b
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-02-07 10:36:21 +0000

    - added ThesaurusControl-bean

---
+++ src/main/java/de/ingrid/utils/ige/profile/beans/controls/ThesaurusControl.java
@@ -0,0 +1,44 @@
+package de.ingrid.utils.ige.profile.beans.controls;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This control generally defines a table with a button where new 
+ * thesaurus entries can be added.
+ * @author Andre
+ *
+ */
+public class ThesaurusControl extends ExtendedControls {
+    
+    private int    numTableRows;
+    private String thesaurusUrl;
+    
+    public ThesaurusControl() {
+        this.setType(THESAURUS_CONTROL);
+    }
+
+    public void setNumTableRows(int numRows) {
+        this.numTableRows = numRows;
+    }
+
+    public int getNumTableRows() {
+        return numTableRows;
+    }
+    
+    public String getThesaurusUrl() {
+        return thesaurusUrl;
+    }
+
+    public void setThesaurusUrl(String thesaurusUrl) {
+        this.thesaurusUrl = thesaurusUrl;
+    }
+    
+    /**
+     * A table has no index name since only its columns can have one!
+     */
+    @Override
+    public String getIndexName() {
+        return null;
+    }
+}
